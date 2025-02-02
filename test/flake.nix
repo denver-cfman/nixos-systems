3fcb10208cb2d7f4f18f3394ef1aad1a6e42bc27
Mr Z <denver.cfman@gmail.com>
2025-02-01 19:33:00 -0700

    Update flake.nix

--- test/flake.nix
+++ test/flake.nix
@@ -1,1 +1,130 @@
-#This Is a test
+#
+# nix flake check --no-build github:denver-cfman/nixos-micro-pi-cluster?ref=main
+# nix flake show --no-build github:denver-cfman/nixos-micro-pi-cluster?ref=main
+#
+{
+  description = "Flake for building a Raspberry Pi Zero 2 SD image";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    deploy-rs.url = "github:serokell/deploy-rs";
+    #colmena.url = "github:zhaofengli/colmena";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    deploy-rs,
+    #colmena,
+    nixos-hardware
+  }@inputs:
+    let
+      # see https://github.com/NixOS/nixpkgs/issues/154163
+      overlays = [
+        (final: super: {
+          makeModulesClosure = x:
+            super.makeModulesClosure (x // { allowMissing = true; });
+        })
+      ];
+      specialArgs = {
+        inherit nixos-hardware inputs;
+      };
+    in rec {
+      nixosConfigurations = {
+         _1b5a4d6b = nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ./1b5a4d6b.nix
+          ];
+        };
+         _004f17e5 = nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [
+            ({ config, pkgs, builtins, ... }: { nixpkgs.overlays = overlays; })
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ./004f17e5.nix
+          ];
+        };
+        fe127cb3 = nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ./fe127cb3.nix
+          ];
+        };
+        _04a91ec3 = nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ./04a91ec3.nix
+          ];
+        };
+        _8d4cb64d = nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = overlays; })
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            ./8d4cb64d.nix
+          ];
+        };
+      };
+
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+
+      deploy = {
+        user = "root";
+        sshOpts = [ "-i" "/home/giezac/.ssh/pzw2.rsa" ];
+        interactiveSudo = true;        
+        # Timeout for profile activation.
+        # This defaults to 240 seconds.
+        activationTimeout = 600;
+      
+        # Timeout for profile activation confirmation.
+        # This defaults to 30 seconds.
+        confirmTimeout = 90;
+        nodes = {
+          _1b5a4d6b = {
+            hostname = "1b5a4d6b";
+            profiles.system.path =
+              deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations._1b5a4d6b;
+            #remoteBuild = true;
+            
+          };
+          _04a91ec3 = {
+            hostname = "04a91ec3";
+            profiles.system.path =
+              deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations._04a91ec3;
+            #remoteBuild = true;
+            
+          };
+          _004f17e5 = {
+            hostname = "004f17e5";
+            profiles.system.path =
+              deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations._004f17e5;
+            #remoteBuild = true;
+            
+          };
+          fe127cb3 = {
+            hostname = "fe127cb3";
+            profiles.system.path =
+              deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.fe127cb3;
+            #remoteBuild = true;
+            
+          };
+          _8d4cb64d = {
+            hostname = "clusterhat";
+            profiles.system.path =
+              deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations._8d4cb64d;
+            #remoteBuild = true;
+            
+          };  
+        };
+      };
+    };
+}
