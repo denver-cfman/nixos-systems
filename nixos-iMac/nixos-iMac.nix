e858bbd72df42c9d3f75efde6b46ecf795f429c6
Mr Z <denver.cfman@gmail.com>
2025-02-28 15:52:46 -0700

    Update nixos-iMac.nix

--- nixos-iMac/nixos-iMac.nix
+++ nixos-iMac/nixos-iMac.nix
@@ -117,6 +117,9 @@
     ];
   };
 
+  users.users.root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPmNXnRi9A/6hQL0wxpyti2Qo+Sd8LZt0uLu/hSJ91tH root@R210ii"
+  ];
 
    programs.wireshark.enable = true;
 
