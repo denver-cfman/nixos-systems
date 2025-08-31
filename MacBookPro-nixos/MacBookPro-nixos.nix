5080d832ed2b65c4f5b7ea06374d72683dcee12e
Mr Z <denver.cfman@gmail.com>
2025-08-31 00:24:46 -0600

    Update MacBookPro-nixos.nix

--- MacBookPro-nixos/MacBookPro-nixos.nix
+++ MacBookPro-nixos/MacBookPro-nixos.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, pinned-nixpkgs, ... }:
+{ config, pkgs, pkgs-termius-9-16-0, ... }:
 
 {
   imports =
