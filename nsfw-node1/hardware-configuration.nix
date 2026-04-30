6acaa1db07b24209c76b3985a19d5c3553c32776
Mr Z <denver.cfman@gmail.com>
2026-04-29 20:07:39 -0600

    Update hardware-configuration.nix

--- nsfw-node1/hardware-configuration.nix
+++ nsfw-node1/hardware-configuration.nix
@@ -13,7 +13,7 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = lib.mkDefault
+  fileSystems."/" = lib.mkForce
     { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
     };
