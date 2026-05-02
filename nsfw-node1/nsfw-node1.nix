5ff068d4aa875c217042f9a1d4347b59d4228f3b
Mr Z <denver.cfman@gmail.com>
2026-05-01 18:08:44 -0600

    Add NFS configuration to nsfw-node1
    Enable NFS services and configure NFS file system for nsfw-storage.

--- nsfw-node1/nsfw-node1.nix
+++ nsfw-node1/nsfw-node1.nix
@@ -60,6 +60,14 @@
     ];
   };
 
+  ### NFS Stuff
+  services.rpcbind.enable = true;
+  fileSystems."/mnt/nsfw-storage" = {
+    device = "192.168.1.250:/mnt/Big10TBPool/HomeLab/nsfw-storage/transmission-temp";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
+  };
+
 
   networking.hostName = "nsfw-node1";
 
