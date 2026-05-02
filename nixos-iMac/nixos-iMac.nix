de1755b1c030b5affc87a920a19f53e08e326a38
Mr Z <denver.cfman@gmail.com>
2026-05-01 18:09:13 -0600

    Update nixos-iMac.nix

--- nixos-iMac/nixos-iMac.nix
+++ nixos-iMac/nixos-iMac.nix
@@ -19,14 +19,6 @@
     efi.canTouchEfiVariables = true;
   };
 
-  ### NFS Stuff
-  services.rpcbind.enable = true;
-  fileSystems."/mnt/nsfw-storage" = {
-    device = "192.168.1.250:/mnt/Big10TBPool/HomeLab/nsfw-storage/transmission-temp";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-
   networking = {
     hostName = "nixos-iMac";
     networkmanager = {
