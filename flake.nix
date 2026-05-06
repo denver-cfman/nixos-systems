56a1440dbbe97bfca0d174594c2bd52eff626423
Mr Z <denver.cfman@gmail.com>
2026-05-05 19:55:10 -0600

    Rename browser-stack-yaml to nsfw-browser-stack-yaml

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
     arion.url = "github:hercules-ci/arion";
     arion.inputs.nixpkgs.follows = "nixpkgs";
-    browser-stack-yaml = {
+    nsfw-browser-stack-yaml = {
       url = "https://gitlab.com/denver.cfman/homelab-container-servers/-/raw/cc4adfe4656c390ef5a2ea089aeff597133a6e31/dell-server-portainer/stacks/nsfw-tor-browser-stack.yaml";
       flake = false;
     };
@@ -30,7 +30,7 @@
     nixos-hardware,
     home-manager,
     arion,
-    browser-stack-yaml
+    nsfw-browser-stack-yaml
   }@inputs:
     let
       # see https://github.com/NixOS/nixpkgs/issues/154163
