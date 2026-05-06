b3b2ab9c166cacbe6d186f5956146d24ca292bab
Mr Z <denver.cfman@gmail.com>
2026-05-05 20:12:44 -0600

    Update containers.nix

--- nsfw-node1/containers.nix
+++ nsfw-node1/containers.nix
@@ -1,5 +1,12 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
+let
+  vncPassword = builtins.getEnv "VNC_PW";
+  finalVncPw = if vncPassword != "" then vncPassword else "password";
+
+  torbImageTag = builtins.getEnv "TORB_IMAGE_TAG";
+  finalTorbImageTag = if torbImageTag != "" then torbImageTag else "aarch64-1.17.0-rolling-daily";
+in
 {
   virtualisation.arion = {
     backend = "podman-socket";
@@ -8,8 +15,12 @@
       settings = {
         services = {
           web-server = {
-            service.image = "mendhak/http-https-echo:latest";
-            service.ports = [ "8080:8080" "8443:8443" ];
+            service.image = "kasmweb/tor-browser:" + finalTorbImageTag;
+            service.ports = [ "6901:6901" ];
+            environment = {
+              VNC_PW = finalVncPw;
+            };
+            shm_size = "512m";
           };
         };
       };
