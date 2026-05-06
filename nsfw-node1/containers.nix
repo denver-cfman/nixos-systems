8b018e834304bc1b60c3ce2262dc60b887292839
Mr Z <denver.cfman@gmail.com>
2026-05-05 18:02:31 -0600

    Switch backend to podman and update web server config

--- nsfw-node1/containers.nix
+++ nsfw-node1/containers.nix
@@ -2,23 +2,14 @@
 
 {
   virtualisation.arion = {
-    backend = "docker"; # Or "podman-socket" if you're using podman
+    backend = "podman-socket";
     
     projects.my-stack = {
       settings = {
-        # This is where your "docker-compose" logic goes, but in Nix syntax
         services = {
           web-server = {
-            service.image = "nginx:latest";
-            service.ports = [ "8080:80" ];
-            service.volumes = [
-              "/var/lib/my-app/html:/usr/share/nginx/html"
-            ];
-          };
-          
-          database = {
-            service.image = "postgres:15";
-            service.environment.POSTGRES_PASSWORD = "password"; # Use sops-nix for real secrets!
+            service.image = "mendhak/http-https-echo:latest";
+            service.ports = [ "8080:8080" "8443:8443" ];
           };
         };
       };
