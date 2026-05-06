4ca737c1d78d5a3eff99b49b10f53260b14832d8
Mr Z <denver.cfman@gmail.com>
2026-05-05 18:54:42 -0600

    Enable NFS mount for nsfw-storage

--- nsfw-node1/nfs-mounts.nix
+++ nsfw-node1/nfs-mounts.nix
@@ -3,11 +3,10 @@
 {
   boot.supportedFilesystems = lib.mkAfter [ "nfs" ];
   services.rpcbind.enable = lib.mkForce true;
-  ### NFS Stuff
-  #services.rpcbind.enable = true;
-  #fileSystems."/mnt/nsfw-storage" = {
-  #  device = "192.168.1.250:/mnt/Big10TBPool/HomeLab/nsfw-storage/transmission-temp";
-  #  fsType = "nfs";
-  #  options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  #};
+
+  fileSystems."/mnt/nsfw-storage" = {
+    device = "192.168.1.250:/mnt/Big10TBPool/HomeLab/nsfw-storage/transmission-temp";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
+  };
 }
