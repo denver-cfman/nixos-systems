cbf68798b8850c313743bc8e688d64ca3e6e8b7e
Mr Z <denver.cfman@gmail.com>
2026-05-09 17:59:08 -0600

    Refactor nsfw-browser service configuration

--- nsfw-node1/containers.nix
+++ nsfw-node1/containers.nix
@@ -12,23 +12,27 @@ in
     backend = "podman-socket";
     projects.arion-container-stack = {
       settings = {
+        # 1. Global Declaration (Forces the 'networks:' header in YAML)
         docker-compose.raw.networks.nsfw-network = {
             name = "NSFW";
             external = true;
         };
-        services = {
-          nsfw-browser = {
-            service = {
-              image = "kasmweb/tor-browser:" + finalTorbImageTag;
-              #ports = [ "6901:6901" ];
-              environment = {
-                VNC_PW = finalVncPw;
-                HTTP_PROXY = "http://10.0.90.3:8118";
-                HTTPS_PROXY = "http://10.0.90.3:8118";
-              };
-              networks = [ "nsfw-network" ];
-              #networks.nsfw-network.ipv4_address = "10.0.90.10";
+
+        services.nsfw-browser = {
+          # 2. Assign the service to the network
+          # This must be outside the 'service' block for some Arion versions
+          # or defined as an attribute set for better compatibility.
+          networks.nsfw-network = {};
+
+          service = {
+            image = "kasmweb/tor-browser:" + finalTorbImageTag;
+            environment = {
+              VNC_PW = finalVncPw;
+              HTTP_PROXY = "http://10.0.90.3:8118";
+              HTTPS_PROXY = "http://10.0.90.3:8118";
             };
+            # 3. If you need a static IP, uncomment this:
+            # networks.nsfw-network.ipv4_address = "10.0.90.10";
           };
         };
       };
