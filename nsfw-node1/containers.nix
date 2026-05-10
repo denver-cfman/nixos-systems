85d1d4e48a4239ee0ee2849eddccdafe3dcff36a
Mr Z <denver.cfman@gmail.com>
2026-05-09 18:15:34 -0600

    Update containers.nix

--- nsfw-node1/containers.nix
+++ nsfw-node1/containers.nix
@@ -7,31 +7,27 @@ let
   finalTorbImageTag = if torbImageTag != "" then torbImageTag else "aarch64-1.17.0-rolling-daily";
 in
 {
-  virtualisation.arion = {
-    backend = "podman-socket";
-    projects.arion-container-stack.settings = {
-      
-      # 1. THE GLOBAL DECLARATION
-      # This MUST be here to create the top-level 'networks' section.
-      docker-compose.raw.networks = {
-        NSFW = {
-          external = true;
-        };
-      };
+{
+  virtualisation.arion.projects.arion-container-stack.settings = {
+    # 1. FORCE GLOBAL NETWORK DEFINITION
+    # This must be a direct child of 'settings'
+    docker-compose.raw.networks.NSFW = {
+      external = true;
+    };
 
-      # 2. THE SERVICE DEFINITION
-      services.nsfw-browser.service = {
-        image = "kasmweb/tor-browser:" + finalTorbImageTag;
-        
-        # Reference the network by the key used above
-        networks = [ "NSFW" ];
-        
-        environment = {
-          VNC_PW = finalVncPw;
-          HTTP_PROXY = "http://10.0.90.3:8118";
-          HTTPS_PROXY = "http://10.0.90.3:8118";
-        };
+    # 2. DEFINE THE SERVICE
+    services.nsfw-browser.service = {
+      image = "kasmweb/tor-browser:" + finalTorbImageTag;
+      
+      # This points to the global 'NSFW' defined above
+      networks = [ "NSFW" ];
+      
+      environment = {
+        VNC_PW = finalVncPw;
+        HTTP_PROXY = "http://10.0.90.3:8118";
+        HTTPS_PROXY = "http://10.0.90.3:8118";
       };
     };
   };
+};
 }
