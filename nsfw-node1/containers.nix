4f86204232d9ae628c5d5833d36a34d443dc996e
Mr Z <denver.cfman@gmail.com>
2026-05-09 18:09:51 -0600

    Update containers.nix

--- nsfw-node1/containers.nix
+++ nsfw-node1/containers.nix
@@ -10,26 +10,23 @@ in
 {
   virtualisation.arion = {
     backend = "podman-socket";
-    projects.arion-container-stack = {
-      settings = {
-        docker-compose.raw.networks.nsfw-network = {
-            name = "NSFW";
-            external = true;
-        };
-        services.nsfw-browser = {
-          # This 'service' block is where Docker-standard options go
-          service = {
-            image = "kasmweb/tor-browser:" + finalTorbImageTag;
-            
-            # 1. Define the network link HERE
-            networks = [ "nsfw-network" ];
-            
-            environment = {
-              VNC_PW = finalVncPw;
-              HTTP_PROXY = "http://10.0.90.3:8118";
-              HTTPS_PROXY = "http://10.0.90.3:8118";
-            };
-          };
+    projects.arion-container-stack.settings = {
+      
+      # 1. Match the key exactly to the 'NAME' in your docker network list
+      docker-compose.raw.networks.NSFW = {
+        external = true;
+      };
+
+      services.nsfw-browser.service = {
+        image = "kasmweb/tor-browser:" + finalTorbImageTag;
+        
+        # 2. Reference the exact name here as well
+        networks = [ "NSFW" ];
+        
+        environment = {
+          VNC_PW = finalVncPw;
+          HTTP_PROXY = "http://10.0.90.3:8118";
+          HTTPS_PROXY = "http://10.0.90.3:8118";
         };
       };
     };
