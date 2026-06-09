bb6bff6b07f046c3082c3f1018281d51d13b4ddc
Mr Z <denver.cfman@gmail.com>
2026-06-08 18:58:53 -0600

    Update hardware-configuration.nix

--- hermes-test1/hardware-configuration.nix
+++ hermes-test1/hardware-configuration.nix
@@ -21,7 +21,7 @@
   #  };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
+    device = lib.mkForce "/dev/disk/by-label/nixos";
     fsType = "ext4";
     neededForBoot = true; 
   };
