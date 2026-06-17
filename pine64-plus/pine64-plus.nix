f2ebeeb4c3d310a63d7914e5ace170e118c571f7
Mr Z <denver.cfman@gmail.com>
2026-06-16 21:17:19 -0600

    Clean up sdImage configuration in pine64-plus.nix
    Removed commands for populating root and firmware in sdImage.

--- pine64-plus/pine64-plus.nix
+++ pine64-plus/pine64-plus.nix
@@ -26,11 +26,6 @@
   sdImage = {
     compressImage = true;
     imageName = lib.mkForce "pine64-plus.img";
-    populateRootCommands = ''
-    mkdir -p ./files/boot
-    ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
-    '';
-    populateFirmwareCommands = "";
   };
 
   hardware.enableRedistributableFirmware = lib.mkForce true;
